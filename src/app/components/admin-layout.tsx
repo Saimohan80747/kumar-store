7a3e5a067ced1861b445aa32d0d513df4ead39a2
mohansai <mohansai152006@gmail.com>
2026-04-11 15:57:49 +0530

    Optimization 7

--- src/app/components/admin-layout.tsx
+++ src/app/components/admin-layout.tsx
@@ -429,4 +429,5 @@ export function AdminLayout() {
  / /   o p t i m i z a t i o n   4  
  / /   o p t i m i z a t i o n   5  
  / /   o p t i m i z a t i o n   6  
+ / /   o p t i m i z a t i o n   7  
  
