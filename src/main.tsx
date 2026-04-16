da5f8d406f127d6972ca4ccf347695fc7025af53
mohansai <mohansai152006@gmail.com>
2026-04-16 16:07:51 +0530

    doc(react): separate global style import comment

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@
   import React from "react";
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
+  // Global styles
   import "./styles/index.css";
 
   // Render the application tree
